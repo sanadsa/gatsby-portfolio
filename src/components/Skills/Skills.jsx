14560d7ae22edae031a458980b6077cc59b8fa61
sanadsa <sanad_s96@hotmail.com>
2020-05-13 04:49:31 +0300

    working on the skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,44 +1,89 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Carousel } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
+import Title from '../Title/Title';
 
-import '../../style/components/_skills.scss';
+import '../../style/sections/_skills.scss';
 
 const Skills = () => {
-  // const [isDesktop, setIsDesktop] = useState(false);
-  // const [isMobile, setIsMobile] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
-  // useEffect(() => {
-  //   if (window.innerWidth > 769) {
-  //     setIsDesktop(true);
-  //     setIsMobile(false);
-  //   } else {
-  //     setIsMobile(true);
-  //     setIsDesktop(false);
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
 
   return (
-    <section id="skills" className="jumbotron">
+    <section id="skills" className="jumbotron-san">
       <Container>
-        <div className="item">
-          <div className="container skills-category">
-            <div className="row skill-title">
-              <h3>Front-end</h3>
-            </div>
-            <div className="row skill-logos">
-              <div className="skill-frame">react</div>
-              <div className="skill-frame">react</div>
-              <div className="skill-frame">react</div>
-              <div className="skill-frame">react</div>
-            </div>
-            <div className="row skill-logos">
-              <div className="skill-frame">react</div>
-              <div className="skill-frame">react</div>
-              <div className="skill-frame">react</div>
-              <div className="skill-frame">react</div>
-            </div>
-          </div>
-        </div>
+        <Title title="My Skills" />
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+          <Carousel>
+            <Carousel.Item>
+              <Row>
+                <Col>
+                  <h3>Front-end</h3>
+                </Col>
+              </Row>
+              <div className="skill-logos">
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+              </div>
+              <div className="skill-logos">
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+              </div>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Row>
+                <Col>
+                  <h3>Back-end</h3>
+                </Col>
+              </Row>
+              <div className="skill-logos">
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+              </div>
+              <div className="skill-logos">
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+              </div>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Row>
+                <Col>
+                  <h3>Other</h3>
+                </Col>
+              </Row>
+              <div className="skill-logos">
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+              </div>
+              <div className="skill-logos">
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+                <div className="skill-frame">HTML</div>
+              </div>
+            </Carousel.Item>
+          </Carousel>
+        </Fade>
       </Container>
     </section>
   );
